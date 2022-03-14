2984b062a0788aa6d9c2f4cc23edfa74a20d8c75
Prasanna <prasannaps2610@gmail.com>
2022-03-14 22:38:32 +0530

    eslint end of line warning changes

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,6 +14,9 @@ module.exports = {
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'prettier/prettier': ['warn', { singleQuote: true }],
+    'prettier/prettier': [
+      'warn',
+      { singleQuote: true, semi: true, endOfLine: 'auto' },
+    ],
   },
 };
